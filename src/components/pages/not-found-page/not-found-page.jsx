9bf8a73d30d3e2c198de6cbfeca144180110d14b
Fumieizu <71212206+Fumieizu@users.noreply.github.com>
2021-10-29 17:01:51 +0500

    deleted useless imports, fixed not-found-page component

--- src/components/pages/not-found-page/not-found-page.jsx
+++ src/components/pages/not-found-page/not-found-page.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-// import styles from './not-found-page.module.scss';
+import Header from '../../elements/header/header';
+import Footer from '../../elements/footer/footer';
+import styles from './not-found-page.module.scss';
 
 
 export default function NotFoundPage() {
   return (
-    <h1>Приносим извинения! Страница находится в разработке.</h1>
+    <>
+      <Header/>
+      <h1 className={styles.title}>Приносим извинения! Страница находится в разработке.</h1>
+      <Footer/>
+    </>
   );
 }
